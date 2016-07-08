ec0670156fb943d66b2e069243ae964e8eebfe9f
David Kunzler <dk@devland.de>
2016-07-08 22:00:48 +0200

    initial import, jack enabled

---
+++ app/src/main/java/de/devland/coder/App.java
@@ -0,0 +1,16 @@
+package de.devland.coder;
+
+import android.app.Application;
+
+/**
+ * Created by deekay on 29.06.2016.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+}
