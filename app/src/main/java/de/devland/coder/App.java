a0d6911203bd4c94a6a023618b1f28cd60052393
David Kunzler <dk@devland.de>
2016-07-09 13:12:10 +0200

    add snippet model and orm

--- app/src/main/java/de/devland/coder/App.java
+++ app/src/main/java/de/devland/coder/App.java
@@ -2,6 +2,9 @@ package de.devland.coder;
 
 import android.app.Application;
 
+import io.realm.Realm;
+import io.realm.RealmConfiguration;
+
 /**
  * Created by deekay on 29.06.2016.
  */
@@ -11,6 +14,10 @@ public class App extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        // Create a RealmConfiguration that saves the Realm file in the app's "files" directory.
+        RealmConfiguration realmConfig = new RealmConfiguration.Builder(this).build();
+        Realm.setDefaultConfiguration(realmConfig);
     }
 
 }
---
+++ app/src/main/java/de/devland/coder/model/Snippet.java
@@ -0,0 +1,23 @@
+package de.devland.coder.model;
+
+import io.realm.RealmObject;
+import io.realm.annotations.PrimaryKey;
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Created by deekay on 09.07.2016.
+ */
+
+@Getter
+@Setter
+public class Snippet extends RealmObject {
+    @PrimaryKey
+    private long id;
+    private String user;
+    private String repository;
+    private String commitId;
+    private int startLine;
+    private int numberOfLines;
+    private int elegance;
+}
